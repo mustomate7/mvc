b2ee7e32eb8c9bc770770e87e2799fc938e913d8
Máté Mustó <mustomate7@gmail.com>
2023-07-10 16:12:58 +0200

    main folders

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+function show($stuff){
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+function splitURL(){
+    $URL = $_GET["url"] ?? "home";
+    $URL = explode("/", $URL);
+    return $URL;
+}
+
+function loadController(){
+    $URL = splitURL();
+
+    $filename = "../app/controllers/" . ucfirst($URL[0]) . ".php";
+    if(file_exists($filename)){
+        require $filename;
+    } else{
+        $filename = "../app/controllers/_404.php";
+        require $filename;
+    }
+}
+
+loadController();
